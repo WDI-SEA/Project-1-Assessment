40382faa78c058fb03447cd92703ec961e28112b
Benji <bwolfenberger@gmail.com>
2021-05-25 14:42:43 -0700

    html skeleton js elements added

---
+++ js/main.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    currentCount = document.getElementById(currentCount)
+    increment = document.getElementById(increment)
+    addButton = document.getElementById(addButton)
+    subtractButton = document.getElementById(subtractButton)
+
+    // document.addEventListener('click', add())
+
+    // function add () {
+
+    // }
+
+    // console.log(subtractButton)
+
+
+
+
+
+})
