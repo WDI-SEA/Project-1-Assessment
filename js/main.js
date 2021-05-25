90c2082abc3d0e4fab6a1ea2f4eae7308bff47a7
Brian Young <berkshirehunt@gmail.com>
2021-05-25 16:56:00 -0700

    DOM references created

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Cache DOM elements
+
+const counter = document.querySelector('.counter')
+const boxPlus = document.querySelector('.boxPlus')
+const boxCenter = document.querySelector('.boxCenter')
+const boxMinus = document.querySelector('.boxMinus')
