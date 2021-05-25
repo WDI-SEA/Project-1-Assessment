7cee8a30917542e85b659a320e7a2ea9ea78e883
funbunch <shannan_bunch@hotmail.com>
2021-05-25 16:12:38 -0700

    Added functionality and styles

--- js/main.js
+++ js/main.js
@@ -3,7 +3,30 @@ document.addEventListener("DOMContentLoaded", () => {
 // DOM 
 const plus = document.querySelector('#plus')
 const minus = document.querySelector('#minus')
-const input = document.querySelector('#input')
-let displayCount = input.value
-console.log(displayCount)
+let input = document.querySelector('#input')
+let displayCount = document.querySelector('#display-count')
+//init
+let count = 1
+
+plus.addEventListener('click', () => {
+  count += parseInt(input.value, 10)
+  plusMinus()
+
+})
+minus.addEventListener('click', () => {
+  count -= parseInt(input.value, 10)
+  plusMinus()
+})
+
+// function to disply add/subtract
+function plusMinus() {
+  displayCount.innerText = count
+  if (displayCount.innerText < 0) {
+    displayCount.style.color = "red"
+  } else {
+    displayCount.style.color = "black"
+  }
+}
+
+
 })
