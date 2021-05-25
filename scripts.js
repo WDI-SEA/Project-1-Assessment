96a62b28d4fe01326ddd97ec07cac6183601caaa
hamazumi <khamamoto12@gmail.com>
2021-05-25 15:20:32 -0700

    initial commit

---
+++ scripts.js
@@ -0,0 +1,28 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+//DOM Variables
+let total = document.querySelector("#total")
+let form = document.querySelector("#form")
+let add = document.querySelector("#add")
+let inputValue = document.querySelector("#inputValue")
+let subtract = document.querySelector("#subtract")
+
+//Variables to be used
+let totalNumber = 0
+
+//Prevent form from refreshing
+function submitForm(event){
+    event.preventDefault()
+}
+inputValue.addEventListener('submit', submitForm);
+
+
+//Function for addition and subtraction
+function math(){
+
+}
+
+
+
+
+})
