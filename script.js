98ad01512f21e3abe9d3903ce3f6d4fcdc9dd562
SpiralV <MVelasco90@hotmail.com>
2021-05-25 16:50:35 -0700

    MVP except my numbers are adding weird

--- script.js
+++ script.js
@@ -2,22 +2,25 @@
 document.addEventListener("DOMContentLoaded", () => {
     let upDog = document.getElementById('plus')
     let downDog = document.getElementById('minus')
-    let numSel = document.getElementById('numval')
+    let toAdd = document.getElementById('addeller')
     let numView = document.getElementById('numdisplay')
 
     // + onclick, numdisplay + input value
     upDog.addEventListener('click', () => {
-        numView.innerText++
+        numView.innerText = toAdd.value + numView.innerText
     })
 
     // - onclick, numdisplay - input value
     downDog.addEventListener('click', () => {
-        numView.innerText--
+        numView.innerText = toAdd.value - numView.innerText
     })
+    console.log(toAdd.value)
 })
-// [ number display ]
-// [ + ] <input>  [ - ]
-// start app
-// numdisplay = 1
-// <input> = 1
-// +- onhover, color invert
+
+
+// [ number display ] - set
+// [ + ] <input>  [ - ] - set
+// start app - it just works
+// numdisplay = 1 - done
+// <input> = 1 - done
+// +- onhover, color invert - css
