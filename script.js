92916e4dd9f6479b1c0b8af50009de0d04ab0edf
BeckyBoo88 <rbooco07@gmail.com>
2021-05-25 16:58:02 -0600

    display done mostly, need to figure out js

---
+++ script.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", () => {
+console.log("Hello! 💕")
+
+// Make Variables
+let outPutNumber = document.getElementById('outPutNumber')
+let addBtn = document.getElementById('add')
+let subBtn = document.getElementById('sub')
+let userInput = document.getElementById('userInput')
+
+
+// Make + button add to div number
+addBtn.addEventListener('click', (event) => {
+    event.preventDefault()
+
+    let outPutNumber.innerText = newTotal
+
+    let newTotal = function addition() {
+        userInput + outPutNumber
+        return sum
+    }
+})
+
+
+//Make - button subtract from div number
+
+
+
+
+
+})
