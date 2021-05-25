b1b3a0a26220d46589e3ffe4c7255706d16fe80d
TERRYSZHOU <TERRYSZHOU@GMAIL.COM>
2021-05-25 16:54:05 -0500

    Basic adding and subtracting functions done. Output done.

---
+++ scripts/script.js
@@ -0,0 +1,21 @@
+console.log("Ey yo")
+
+const negButton = document.getElementById("negative-button")
+const posButton = document.getElementById("positive-button")
+const numberInput = document.getElementById("number-input")
+const numberOutput = document.getElementById("output-box")
+
+// console.log(numberOutput)
+// console.log(numberInput)
+// console.log(parseInt(numberOutput.innerText))
+
+
+posButton.addEventListener("click", () => {
+    console.log("It goes up!")
+    numberOutput.innerText = parseInt(numberOutput.innerText) + parseInt(numberInput.value)
+})
+
+negButton.addEventListener("click", () => {
+    console.log("It goes down!")
+    numberOutput.innerText = parseInt(numberOutput.innerText) - parseInt(numberInput.value)
+})
