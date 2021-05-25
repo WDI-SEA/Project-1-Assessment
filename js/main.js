6e4f19bea0779b45244ab7795f7171a1f63bbb5f
Benji <bwolfenberger@gmail.com>
2021-05-25 16:08:58 -0700

    stylized and added full functionality

--- js/main.js
+++ js/main.js
@@ -1,17 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    let currentCount = document.getElementById('currentCount')
+    let currentCountBox = document.getElementById('currentCountBox')
     let increment = document.getElementById('increment')
     let addButton = document.getElementById('addButton')
     let subtractButton = document.getElementById('subtractButton')
+    let currentCount = 0
+    
 
-    // document.addEventListener('click', add())
+    addButton.addEventListener('click', add)
+    subtractButton.addEventListener('click', subtract)
 
-    // function add () {
 
-    // }
-
-    console.log(currentCount)
+    function add () {
+        currentCount +=  +increment.value
+        currentCountBox.innerText = currentCount
+        if (currentCount < 0) {
+            currentCountBox.style.color = 'red'
+        } else if (currentCount > 0) {
+            currentCountBox.style.color = 'green'
+        } else {
+            currentCountBox.style.color = 'black'
+        }
+    }
+    
+    function subtract () {
+        currentCount -=  +increment.value
+        currentCountBox.innerText = currentCount
+        if (currentCount < 0) {
+            currentCountBox.style.color = 'red'
+        } else if (currentCount > 0) {
+            currentCountBox.style.color = 'green'
+        } else {
+            currentCountBox.style.color = 'black'
+        }
+    }
 
 
 
