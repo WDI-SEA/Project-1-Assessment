060a6695b6ba39d396e632ae05c701df5fab2805
hamazumi <khamamoto12@gmail.com>
2021-05-25 16:52:06 -0700

    Finished Assessment

--- scripts.js
+++ scripts.js
@@ -4,25 +4,29 @@ window.addEventListener('DOMContentLoaded', () => {
 let total = document.querySelector("#total")
 let form = document.querySelector("#form")
 let add = document.querySelector("#add")
-let inputValue = document.querySelector("#inputValue")
 let subtract = document.querySelector("#subtract")
 
 //Variables to be used
 let totalNumber = 0
 
-//Prevent form from refreshing
-function submitForm(event){
-    event.preventDefault()
-}
-inputValue.addEventListener('submit', submitForm);
 
+//Function for addition and subtraction then .innerHTML
 
-//Function for addition and subtraction
-function math(){
+add.addEventListener('click', (e) => {
+    e.preventDefault()
+    let inputValue = parseInt(document.querySelector("#inputValue").value)
+    let sum
+    sum = totalNumber += inputValue
+    total.innerHTML = sum
+    console.log(sum)
+})
 
-}
-
-
-
+subtract.addEventListener('click', (e) => {
+    e.preventDefault()
+    let inputValue = parseInt(document.querySelector("#inputValue").value)
+    let diff
+    diff = totalNumber -= inputValue
+    total.innerHTML = diff
+})
 
 })
