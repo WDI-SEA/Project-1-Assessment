f2dc5c03966335b4e07a38fff7d295ce7e4cf3bc
funbunch <shannan_bunch@hotmail.com>
2021-05-25 14:51:28 -0700

    Added initial setup

---
+++ js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+// DOM 
+const plus = document.querySelector('#plus')
+const minus = document.querySelector('#minus')
+const input = document.querySelector('#input')
+let displayCount = input.value
+console.log(displayCount)
+})
