5a19edda38e3d3397461e21aab226a0673034969
rei-bp <reinard.bp@gmail.com>
2021-05-25 17:50:00 -0700

    solve text field grab

--- script.js
+++ script.js
@@ -1,30 +1,52 @@
-let amount = 0
-let deltaNum = 1
 let plus = document.getElementById('plus')
 let minus = document.getElementById('minus')
 let deltaDom = document.getElementById('deltaNum')
 let button = document.querySelector('button')
+let amountDisplay = document.getElementById('numberBox').innerText
+let userInput = deltaDom.value
+let reduceArr
 
 
-function add (deltaNum) {
-    amount + deltaNum
-    return amount
+finalAmount = [0]
+
+const reducer = (a, b) => (a + b)
+
+document.addEventListener('click', (e) => {
+    clickHandler(e)
+})
+
+function displayAmount () {
+   reduceArr = finalAmount.reduce(reducer)
+   console.log(reduceArr)
 }
 
-function subtract (deltaNum) {
-    amount - deltaNum
-    return amount
-}
+
+
 
 function clickHandler (e) {
+    if (e.target === plus) {
+        finalAmount.push(parseInt(deltaDom.value))
+        displayAmount()
+        console.log(finalAmount)
+    }
     if (e.target === minus) {
         console.log('minus')
     }
-    if (e.target === plus) {
-        console.log('plus')
-    }
 }
 
-document.addEventListener('click', (e) => {
-clickHandler(e)
-})
+
+// let amount = document.querySelector('numberBox').innerText
+// let deltaNum = 1
+//     deltaNum = userInput
+
+// function adjustHandler (input) {
+//     input = UserInput
+//     add(input)
+//     subtract(input)
+// }
+
+
+// function subtract (deltaNum) {
+//     amount - deltaNum
+//     return amount
+// }
