2f823e0b45ad38ea486828c70338e189ff49a370
rei-bp <reinard.bp@gmail.com>
2021-05-25 18:50:37 -0700

    fix value centering - ready for turn in

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ let deltaDom = document.getElementById('deltaNum')
 let amountDisplay = document.getElementById('numberBox')
 let reduceArr
 
-
 finalAmount = [0]
 
 const reducer = (a, b) => (a + b)
@@ -17,7 +16,6 @@ function displayAmount () {
    reduceArr = finalAmount.reduce(reducer)
    amountDisplay.innerText = reduceArr
 }
-
 
 function clickHandler (e) {
     if (e.target === plus) {
