ef6ef91bdf51057cdc75acdbee70295411db0720
hannahlegros <hannahrlegros@gmail.com>
2021-10-29 17:02:01 -0400

    welp...

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 // declare vars & grab relevant items from html
-
-let answerDisplay = document.getElementById("display")
+const answerDisplay = document.getElementById("display")
 const minusButton = document.getElementById("minus")
 const plusButton = document.getElementById("plus")
-let inputNumber = document.getElementById("inputbox")
+const inputBox = document.getElementById("input")
 
-
+answerDisplay.innerText = 0
 // write function that subtracts input from display number and displays result
+const subtractNumber = () => {
+    answerDisplay.innerText = (answerDisplay.innerText)-(inputBox.innerText)
+}
 
 // write function that adds input to display number and displays result
-
+const addNumber = () => {
+    answerDisplay.innerText = (answerDisplay.innerText)+(inputBox.innerText)
+}
 
 // create click event listener for minus
-minusButton.addEventListener("click", subtractNumber())
+minusButton.addEventListener("click", subtractNumber)
 // create click event listener for plus
-plusButton.addEventListener("click", addNumber())
+plusButton.addEventListener("click", addNumber)
