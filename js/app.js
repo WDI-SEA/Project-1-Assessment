31161b6433fe767c0501d44b6214b7e902f5548c
iwaggoner <iwaggoner95@gmail.com>
2021-10-29 16:19:37 -0400

    Added bouns and changed some colors

--- js/app.js
+++ js/app.js
@@ -4,17 +4,22 @@ let number = parseInt(document.getElementById('number').innerText)
 const plus = document.getElementById('plus')
 const minus = document.getElementById('minus')
 const form = document.querySelector('form')
+document.getElementById('input').value = 0
 
 
 
 plus.addEventListener('click', () => {
-
     number = number + parseInt(document.getElementById('input').value)
     document.getElementById('number').innerText = number
-
+    if(number < 0 ){
+        document.getElementById('number').style.color = 'red'
+    }
 })
 
 minus.addEventListener('click', () => {
     number = number - parseInt(document.getElementById('input').value)
     document.getElementById('number').innerText = number
+    if(number < 0 ){
+        document.getElementById('number').style.color = 'red'
+    }
 })
