0eea02cb4aabf5d51d9a13b6c9cfbc18feb195f8
Goro <goro.ikeda.iyeki@gmail.com>
2021-10-29 15:24:33 -0400

    Finished Assignment, going to pursue the bonus features.

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let plusSignBtn = document.querySelector("#plus-sign")
+let minusSignBtn = document.querySelector("#minus-sign")
+let inputBox = document.querySelector("input")
+let displayText = document.querySelector("#large-indicator")
+
+plusSignBtn.addEventListener("click", upIncrementBy)
+minusSignBtn.addEventListener("click", downIncrementBy)
+
+function downIncrementBy (e) {
+    let subtractForm = parseInt(displayText.innerText)
+    let newDisplay = subtractForm -= inputBox.value
+    displayText.innerText = newDisplay
+    console.log("click")
+}
+
+function upIncrementBy (e) {
+    e.preventDefault()
+    let addForm = parseInt(displayText.innerText)
+    let newDisplay2 = addForm += parseInt(inputBox.value)
+    displayText.innerText = newDisplay2
+    console.log("click")
+}
+
