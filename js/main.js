122e15c1b94fd623356d176302b2b6d8db44012d
dinosaur96m <nccadmantinaos@gmail.com>
2021-10-29 16:50:27 -0400

    styling nearly identical, bonus achieved

--- js/main.js
+++ js/main.js
@@ -1,49 +1,33 @@
-// Define variables that hold application state, cache DOM elements, hold lookup data (constants), etc.
 // Select elements in the DOM.
 let countDisplay = document.getElementById("countDisplay")
 const plus = document.getElementById("plus")
 const minus = document.getElementById("minus")
 let inputBox = document.getElementById("inputBox")
-// Manipulate a DOM element's content and style.
 
-console.log("Hello HTML")
-
-
-
-// When the "+" button is clicked, 
-//the value entered in the <input> is 
-// added to the count and the new count value displayed.
-
-// When the "-" Button is Clicked
-// he value entered in the <input> is 
-// subtracted from the count and the new count value displayed.
-
-// Hovering Over the "+" or "-" Button
-// When the mouse is over the "+" or "-" button, the button should reverse its background and text colors:
-
-// As a bonus, display the count in red if it is a negative value!
-
-
-
-
-
-
-// Listen for browser events, such as click, being dispatched by DOM elements.
-document.addEventListener("mouseover", (e) => {
-    if (e.target.innerText === "+" || e.target.innerText === "-") {
-
+const textColor = () => {
+    //display text should be red if value is negative
+    if (countDisplay.innerText < 0 ) {
+        countDisplay.style.color = "red"
+    //display text should be black if value is zero or positive
+    } else if (countDisplay.innerText >= 0) {
+        countDisplay.style.color = "black"
     }
-})
-
-
+}
 
 document.addEventListener("click", (e) => {
+    //avoid concatenation by conveting values to ints
     let absoluteValue = parseInt(countDisplay.innerText, 10)
     let input = parseInt(inputBox.value, 10)
-
+    //find out which button was clicked
     if (e.target.id === "plus") {
+    //increase displayed value by number inside of input
     countDisplay.innerText =  absoluteValue + input
+    //ensure text is correct color
+    textColor()
     } else if (e.target.id === "minus") {
+    //decrease displayed value by number inside of input
     countDisplay.innerText = absoluteValue - input
+    //ensure text is correct color
+    textColor()
     }
 })
