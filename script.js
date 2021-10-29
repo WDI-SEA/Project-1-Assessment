e2567fc528eac5182ec40039e01aeaa4bca99570
isaac8069 <isaac8069@gmail.com>
2021-10-29 16:50:45 -0400

    text input has me stumped

---
+++ script.js
@@ -0,0 +1,39 @@
+/// Variables ///
+const minus = document.getElementById("minus")
+const plus = document.getElementById("plus")
+const display = document.getElementById("display")
+
+let count = 0
+updateDisplay()
+
+/// Variables Ended ///
+
+/// eventListeners ///
+minus.addEventListener("click", function() {
+    count--
+    updateDisplay()
+})
+
+plus.addEventListener("click",function() {
+    count++
+    updateDisplay()
+})
+//// eventListeners Ended ///
+
+/// functionality ///
+display.innerText = `${count}`
+function updateDisplay() {
+    // count += 1
+    display.innerText = `${count}`
+    // console.log(count)
+    if(display)
+    /// How to make a negative number red ///
+
+    if(count < 0) {
+        display.style.color = "red"
+    } else {
+        display.style.color = "black"
+    }
+}
+
+/// Functionality Ended ///
