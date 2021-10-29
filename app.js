e5b9940d4ea50429a1cdab306f95741810586e30
cmiltiadou <cmiltiad@gmail.com>
2021-10-29 16:08:48 -0400

    completed assesment

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+
 document.addEventListener('DOMContentLoaded', ()=>{
-    console.log("we live")
+    // form.addEventListener('submit', (e) =>
+    // e.preventDefault()
+    // )
+    let count = 0
+    countDisplay.innerText = count
+
+    subtractButton.addEventListener('click', (e) => {
+        count = count - inputField.value
+        countDisplay.innerText = count
+    })
+    addButton.addEventListener('click', (e) => {
+        count += parseInt(inputField.value) 
+        countDisplay.innerText = count
+    })
 })
