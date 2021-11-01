37dd168ec36de5f3489e32ce2d64428379ac6f8d
hannahlegros <hannahrlegros@gmail.com>
2021-11-01 00:40:20 -0400

    functioning

--- js/main.js
+++ js/main.js
@@ -6,14 +6,14 @@ console.log(answerDiv)
 answerDiv.value = 0
 // write function that subtracts input from display number and displays result
 const subtractNumber = () => {
-    answerDiv.value = (answerDiv.value) - (inputBox.value)
+    answerDiv.value = (+answerDiv.value) - (+inputBox.value)
     console.log(answerDiv.value)
     console.log(inputBox.value)
 }
 
 // write function that adds input to display number and displays result
 const addNumber = () => {
-    answerDiv.value = (answerDiv.value)+=(inputBox.value)
+    answerDiv.value = (+answerDiv.value)+(+inputBox.value)
 }
 
 // create click event listener for minus
