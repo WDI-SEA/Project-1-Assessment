5ebf379287c50cdb99817dc124ce295ddbbd9e67
hannahlegros <hannahrlegros@gmail.com>
2021-11-01 00:27:26 -0400

    styling better, functionality almost there

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 // declare vars & grab relevant items from html
-const answerDisplay = parseInt(document.getElementById("display").innerText)
-const minusButton = document.getElementById("minus")
-const plusButton = document.getElementById("plus")
-const inputBox = parseInt(document.getElementById("input").innerText)
+const answerDiv = document.getElementById("display")
+const inputBox = document.getElementById("input")
 
-display.innerText = 0
+console.log(answerDiv)
+answerDiv.value = 0
 // write function that subtracts input from display number and displays result
 const subtractNumber = () => {
-    display.innerText = (answerDisplay.innerText)-(inputBox.innerText)
+    answerDiv.value = (answerDiv.value) - (inputBox.value)
+    console.log(answerDiv.value)
+    console.log(inputBox.value)
 }
 
 // write function that adds input to display number and displays result
 const addNumber = () => {
-    display.innerText = (answerDisplay.innerText)+(inputBox.innerText)
+    answerDiv.value = (answerDiv.value)+=(inputBox.value)
 }
 
 // create click event listener for minus
-minusButton.addEventListener("click", subtractNumber)
+document.getElementById("minus").addEventListener("click", subtractNumber)
 // create click event listener for plus
-plusButton.addEventListener("click", addNumber)
+document.getElementById("plus").addEventListener("click", addNumber)
