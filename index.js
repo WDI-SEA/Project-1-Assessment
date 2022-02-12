77a81497377d07ae3a61c487e996d8447a5271ef
justin-aquino <justin.aquino25@gmail.com>
2022-02-11 16:40:22 -0800

    submitting complete Project 1 assessment

---
+++ index.js
@@ -0,0 +1,27 @@
+let numberDisplay = document.getElementById("numberDisplay")
+const decrement = document.getElementById("minus")
+const increment = document.getElementById("plus")
+const inputDisplay = document.getElementById("numInput")
+let number = 0
+numberDisplay.innerText = number
+
+inputDisplay.addEventListener("input", ()=>{
+    console.log(inputDisplay.value)
+})
+
+decrement.addEventListener("click", () => {
+    let num = inputDisplay.value
+    number -= num * 1
+    numberDisplay.innerText = number
+
+    if (number < 0) {
+        numberDisplay.style.color = "red"
+    }
+})
+
+increment.addEventListener("click", () => {
+    let num = inputDisplay.value
+    number += num * 1
+    numberDisplay.innerText = number
+    // inputDisplay.innerText =""
+})
