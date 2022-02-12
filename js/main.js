e8ae545390f3e5bda118cc1d78a9c0ffba128170
kokopuffz <chanheeya@gmail.com>
2022-02-11 22:48:19 -0600

    working js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,116 @@
 console.log("im on a boat")
-//gets the number put in by person
+
+
 let inputNum = document.getElementById('input-num').value
-
-//add listener for this button
 let addButton = document.getElementById('add-button')
-
-//add listener for this button
 let minusButton = document.getElementById('minus-button')
-
-//change this depending on what is clicked
-let display = document.getElementById('display-num')
+let displayNum = document.getElementById('display-num')
 let storedNum = inputNum
+let num = null
 
-//i need to get the number every time to check if it changed
-console.log(storedNum)
-let  getCurrentNum = () => {
-  if (storedNum !== inputNum){
-    storedNum =inputNum
+let checkChange = () => {
+  let newNum = document.getElementById('input-num').value
+  if (storedNum === newNum){
+    console.log('same!')
   }else{
-    return
+    storedNum = newNum
+    num = storedNum
+    console.log('newnum!')
   }
 }
 
+// addButton.addEventListener('click', add => {
+//   console.log('rar')
+//   checkChange()
+//   console.log(storedNum)
+//   console.log(num)
+// })
 
-// // let displayNum = 
 
-addButton.addEventListener("click", () =>{
-  getCurrentNum()
-  storedNum++
-  display.innerText(storedNum)
-  // parseNum(inputNum)++÷
+let display = () => {
+  displayNum.innerText = num
+}
+minusButton.addEventListener('click', minus => {  
+  checkChange();
+  console.log(num)
+  num--
+  display()
 })
+
+addButton.addEventListener('click', add => {  
+  checkChange();
+  console.log(num)
+  num++
+  display()
+
+})
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  num = 0
+  // minusButton.addEventListener('click', minus)
+})
+
+
+// any button is clicked u get value of inputNum and store it 
+
+// if inputnumber is different than stored num... store that num as the input number
+
+
+
+
+
+// //gets the number put in by person
+// let inputNum = null
+// //add listener for this button
+// let addButton = document.getElementById('add-button')
+
+// //add listener for this button
+// let minusButton = document.getElementById('minus-button')
+
+// //change this depending on what is clicked
+// let display = document.getElementById('display-num')
+// let storedNum = null
+// let changedNum = Number(storedNum)
+
+
+
+
+
+// let getInputNum = () => {
+//   console.log('rar')
+//   inputNum = document.getElementById("input-num").value;
+//   if (inputNum !== storedNum){
+//     storedNum = inputNum 
+//   } else {
+//     return
+//   }
+// }
+
+// let add = () => {
+//   changedNum++
+// }
+
+
+// addButton.addEventListener = ("click", add => {
+//   if (inputNum !== storedNum){
+
+  // }
+// })
+
+
+
+//i need to get the number every time to check if it changed
+
+// // // let displayNum = 
+
+// addButton.addEventListener("click", () =>{
+//   getInputNum()
+//   console.log(newNum++)
+  // storedNum++
+  // display.innerText(storedNum)
+  // parseNum(inputNum)++÷
+// })
 
 // minusButton.addEventListener("click", () =>{
 //   getCurrentNum()
