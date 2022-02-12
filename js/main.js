9c0fbe779a24412127bed3db32fbffd6d402f4d5
Professorsurf <guevara.a.gabriel@gmail.com>
2022-02-11 17:16:33 -0800

    will be back to finish shortly

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const addUp = document.querySelector('.up');
+const subtractDown = document.querySelector('down')
+const addUp = document.querySelector(".up");
+const subtractDown = document.querySelector(".down");
+
+let addOne = document.getElementByID('.counterdisplay');
+myElem.onclick = function() {
+	(ccounter.addOne) ++
+}
