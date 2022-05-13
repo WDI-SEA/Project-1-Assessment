a2557e2f0f4e254d16dd42af650af20217229ba7
“erhaneth” <gumusucb21@gmail.com>
2022-05-13 16:13:26 -0700

    Project 1 assement

---
+++ js/app.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function(){
+    
+    const p = document.querySelector('p')
+    p.style.backgroundColor = "black"
+    p.style.fontSize = "55px"
+
+    document.querySelector('btn-plus').addEventListener('click', function(){
+        console.log('here')
+
+
+
+    })
+    
+    
+    document.querySelector('btn-minus').addEventListener('click', function(){
+        console.log('here')
+
+
+
+    })
+        
+
+
+})
