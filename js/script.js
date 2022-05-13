e4c620ee8acaf881f2dad6567398623bf5d600ce
Billy Lu <billyfwlu@gmail.com>
2022-05-13 15:22:01 -0700

    completed assignment

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
+const plusBtn = document.querySelector("#plusBtn")
+const numInput = document.querySelector("#numInput")
+const minusBtn = document.querySelector("#minusBtn")
+const display = document.querySelector("#display")
+
+let displayVal = 0
+
+
+
+
 document.addEventListener("DOMContentLoaded", function(){
-    console.log("hi")
+    display.innerText = Number(displayVal) 
+    plusBtn.addEventListener("click",addToDisplay)
+    minusBtn.addEventListener("click",subtractToDisplay)
 })
+
+function addToDisplay() {      
+    displayVal = displayVal + Number(numInput.value)
+    negativeCheck(displayVal)
+    display.innerText = displayVal
+}
+
+function subtractToDisplay() {
+    displayVal = displayVal - Number(numInput.value)
+    negativeCheck(displayVal)
+    display.innerText = displayVal
+}
+
+function negativeCheck(num){
+    if (num < 0 ){
+        display.classList.add("negNum")
+    } else {
+        display.classList.remove("negNum")
+    }
+}
