29d6aad7b9ce46b85eb50bf53ff0226c028b926b
Eduarte783 <eric.duarte83@gmail.com>
2022-05-13 16:11:27 -0700

    assessment 1

---
+++ js/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+let button = document.getElementById("button");
+let number1 = document.getElementById("number1");
+let number2 = document.getElementById("number2");
+
+function add() {
+  document.
+}
+
+  });
+
+/*let num1 = 5;
+let num2 = 7;
+
+console.log(num1 + num2)*/
